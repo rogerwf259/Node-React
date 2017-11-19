a830bdf97f97f8b0fbc8339d517f07fe1bb2d29c
Roger <rogeliomer_2@hotmail.com>
2017-11-18 19:06:16 -0600

    Second commit

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const app = express.createapp(app);
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'buddy' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
